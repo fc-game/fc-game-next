f16eada3b1762ead19c7d35a2597c06d81dd7e1e
RyuSeiri <56245152+RyuSeiri@users.noreply.github.com>
2026-02-28 18:50:44 +0900

    #2 #1

--- src/app/run/[slug]/page.tsx
+++ src/app/run/[slug]/page.tsx
@@ -1,6 +1,17 @@
 import RunOnlyClient from "../RunOnlyClient";
+import config from "@/src/config";
 
-export default async function RunSlugPage({ params }: any) {
-  const resolvedParams = await params;
+type RunSlugPageProps = {
+  params: Promise<{ slug: string }> | { slug: string };
+};
+
+export function generateStaticParams() {
+  return Object.keys(config.ROMS).map((slug) => ({ slug }));
+}
+
+export const dynamicParams = false;
+
+export default async function RunSlugPage({ params }: RunSlugPageProps) {
+  const resolvedParams = await Promise.resolve(params);
   return <RunOnlyClient slug={resolvedParams?.slug} />;
 }
